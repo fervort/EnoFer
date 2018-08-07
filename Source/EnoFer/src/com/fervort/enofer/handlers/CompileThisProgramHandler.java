a813c776c013cf5c41a7f4def10307e3335ad5ad
fervort <fervort.com@gmail.com>
2018-08-07 18:52:36 +0200

    Added handler for Import compile and extract program

---
+++ Source/EnoFer/src/com/fervort/enofer/handlers/CompileThisProgramHandler.java
@@ -0,0 +1,91 @@
+package com.fervort.enofer.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IMarkSelection;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+
+public class CompileThisProgramHandler extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+
+        if( window != null )
+        {
+            ISelection selection = (ISelection) window.getSelectionService().getSelection();
+            
+             
+            if (selection instanceof IStructuredSelection) {
+    			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+    			if(structuredSelection.size()!=1)
+    			{
+    				MessageDialog.openError(window.getShell(),"EnoFer Error","Wrong Selection. You have selected "+structuredSelection.size()+" Componenets. Please select only one component at a time.");
+    			}else
+    			{
+    				Object obFirstSelection = structuredSelection.getFirstElement();
+    				if(obFirstSelection instanceof org.eclipse.jdt.core.IJavaProject)
+    				{
+    					MessageDialog.openInformation(window.getShell(), "EnoFer Info", "Wrong Selection. This is project. Select program and then click this option.");
+    				}else if(obFirstSelection instanceof org.eclipse.jdt.core.IPackageFragmentRoot)
+    				{
+    					MessageDialog.openInformation(window.getShell(), "EnoFer Info", "Wrong Selection. This is Source Folder. Select program and then click this option.");
+    				}else if(obFirstSelection instanceof org.eclipse.jdt.core.IPackageFragment)
+    				{
+    					MessageDialog.openInformation(window.getShell(), "EnoFer Info", "Wrong Selection. This is package. Select program and then click this option.");
+    				}
+    				else if(obFirstSelection instanceof org.eclipse.jdt.core.ICompilationUnit)
+    				{
+    					//MessageDialog.openInformation(window.getShell(), "EnoFer Info", "This is file");
+    					
+    					IResource selectedResource = (IResource)((IAdaptable)obFirstSelection).getAdapter(IResource.class);
+    					MessageDialog.openInformation(window.getShell(), "EnoFer Info","This is file"+selectedResource.getLocation());
+    				}else
+    				{
+    					MessageDialog.openInformation(window.getShell(), "EnoFer Info", "Wrong Selection. Select program and then click this option.\n"+obFirstSelection);
+    					System.out.print("Found uncategories class to cast: "+obFirstSelection);
+    					System.out.print(obFirstSelection);	
+    				}
+    				
+    			}
+    			
+    		}
+    		if (selection instanceof ITextSelection) {
+    			MessageDialog.openError(window.getShell(),"EnoFer Error","Wrong Selection. You have selected text .You have to select Program to compile.");
+    			//ITextSelection ts  = (ITextSelection) selection;
+    			//System.out.println(ts.getText());
+    			//System.out.println("ITextSelection "+ts.getText());
+    		}
+    		if (selection instanceof IMarkSelection) {
+    			MessageDialog.openError(window.getShell(),"EnoFer Error","Wrong Selection. You have selected text .You have to select Program to compile.");
+    			/*
+    			IMarkSelection ms = (IMarkSelection) selection;
+    			System.out.println("IMarkSelection "+ms.getDocument());
+    			try {
+    			    System.out.println(ms.getDocument().get(ms.getOffset(), ms.getLength()));
+    			    
+    			} catch (BadLocationException ble) { }
+    			*/
+    		}
+    		
+        }
+		
+		return null;
+	}
+
+}
